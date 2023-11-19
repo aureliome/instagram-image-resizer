003d383cf3068a5dd1c042a6171c1d78b96ba03c
Aurelio Merenda <aurelio.merenda@musement.com>
2023-11-19 22:04:41 +0100

    test: exclude src/inc/constants from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  collectCoverageFrom: ["src/**/*.ts"],
+  collectCoverageFrom: ["src/**/*.ts", "!src/inc/constants.ts"],
   preset: "ts-jest",
   testEnvironment: "node",
 };
