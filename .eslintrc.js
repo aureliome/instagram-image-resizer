9cc1e94347404aaa5f0fd455a3f689d4c2b97090
Aurelio Merenda <aurelio.merenda@musement.com>
2023-11-29 18:07:47 +0100

    chore: installed prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,16 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:prettier/recommended"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "prettier"],
   root: true,
+  rules: {
+    "prettier/prettier": "error"
+  }
 };
