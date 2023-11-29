79b1cf069f70273e93a76b58e428f08d4a6dea4a
Aurelio Merenda <aurelio.merenda@musement.com>
2023-11-29 17:21:16 +0100

    chore: added linting with type information

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 /* eslint-env node */
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+  ],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   plugins: ["@typescript-eslint"],
   root: true,
 };
