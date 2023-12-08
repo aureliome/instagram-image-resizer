f5633add5a7b462dd191fdd4547973cd1a96f2c5
Aurelio Merenda <aurelio.merenda@musement.com>
2023-12-08 14:20:26 +0100

    test(integration): first test on hor image completed

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,29 +1,60 @@
 import { promises as fs } from "fs";
+import sharp from "sharp";
 import main from "../src/index";
+
+const sourceFolder = "test/files/src";
+const destFolder = "test/files/dest";
+const expectedFolder = "test/files/expected";
+
+const compareImage = async (fileName) => {
+  // check sizes
+  const { width: destWidth, height: destHeight } = await sharp(
+    `${destFolder}/${fileName}`
+  ).metadata();
+
+  const { width: expectedWidth, height: expectedHeight } = await sharp(
+    `${expectedFolder}/${fileName}`
+  ).metadata();
+  expect(destWidth).toBe(expectedWidth);
+  expect(destHeight).toBe(expectedHeight);
+
+  // check data
+  const destImage = await fs.readFile(`${destFolder}/${fileName}`);
+  const expectedImage = await fs.readFile(`${expectedFolder}/${fileName}`);
+  expect(destImage).toEqual(expectedImage);
+};
 
 describe("integration tests", () => {
   afterEach(async () => {
-    await fs.rm("test/files/dest", { recursive: true });
-    console.log("clean");
+    await fs.rm(destFolder, { recursive: true });
   });
 
   describe("resize a single image", () => {
     it("resize an horizontal image", async () => {
-      await main("test/files/src/horizontal1.jpeg", "test/files/dest");
+      await main(`${sourceFolder}/horizontal1.jpeg`, destFolder);
+
+      const destFiles = [
+        "horizontal1-1.jpg",
+        "horizontal1-2.jpg",
+        "horizontal1-3.jpg",
+      ];
+      for (const destFile of destFiles) {
+        await compareImage(destFile);
+      }
     });
 
-    it("resize a vertical image", async () => {
-      await main("test/files/src/vertical1.jpeg", "test/files/dest");
+    xit("resize a vertical image", async () => {
+      await main(`${sourceFolder}/vertical1.jpeg`, destFolder);
     });
 
-    it("resize a square image", async () => {
-      await main("test/files/src/square.jpeg", "test/files/dest");
+    xit("resize a square image", async () => {
+      await main(`${sourceFolder}/square.jpeg`, destFolder);
     });
   });
 
-  describe("resize multiple images inside a folder", () => {
-    it("resize a folder with multiple images", async () => {
-      await main("test/files/src/", "test/files/dest");
+  xdescribe("resize multiple images inside a folder", () => {
+    xit("resize a folder with multiple images", async () => {
+      await main(`${sourceFolder}/`, destFolder);
     });
   });
 });
